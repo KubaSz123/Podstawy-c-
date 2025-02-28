c266056eb197a404a03d96f3e97799e93391eb09
Jakub Szczepański <kubutor@gmail.com>
2025-02-28 13:06:10 +0100

    koniec zajęć

---
+++ Zadanie 6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Napisać program wczytujący dwie liczby i wypisujący na ekranie ich sumę, różnicę, iloczyn i iloraz.
+            Double a, b;
+            Console.Write("Podaj liczbe a");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Podaj liczbe b");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("suma={0}", a+b);
+            Console.WriteLine("róznica={0}", a - b);
+            Console.WriteLine("iloczyn={0}", a * b);
+            Console.WriteLine("iloraz={0}", a / b);
+            Console.ReadKey(true);
+
+
+        }
+    }
+}
---
+++ zadaniee 5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadaniee_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Napisać program wczytujący promień podstawy (r) i wysokość (h)
+            // stożka i wypisujący na ekranie objętość i pole całkowite tego stożka. 
+            Double r, h, l, V, Pc;
+            Console.Write("Podaj promień") ;
+            r = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Podaj wysokość");
+            h = Convert.ToDouble(Console.ReadLine());
+            l = Math.Sqrt(r * r + h * h);
+            V = 1.0 / 3 * Math.PI * r * r * h;
+            Pc = Math.PI * r * r + Math.PI * r * 1;
+            Console.WriteLine("objętość={0}", V);
+            Console.WriteLine("Pole={0}", Pc);
+            Console.ReadKey (true);
+        }
+    }
+}
