95a8ba255d371c1fb19696265814186dff9f59d9
KIE_Admin <KIE_Admin@WZR-C24-15>
2025-04-07 15:31:37 +0200

    koniec metody 07.04.2025

---
+++ zadanie 2 metody/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace zadanie_2_metody
+{
+    //Napisz funkcję o nazwie max, która wyznacza maksimum z dwóch liczb rzeczywistych
+    //zadanych jako argumenty tej funkcji.Napisz program wykorzystujący funkcję max.
+    internal class Program
+    {
+        static double max(double a, double b)
+        { if (a > b)
+                return a;
+            else return b;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Maksymalna wartosc z 10 i 24 to {0}", max(10, 24));
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ zadanie 3 metody/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_3_metody
+{
+    internal class Program
+    {
+        //Napisz funkcję o nazwie pole_kola, która posiada jeden parametr r-promień koła i zwraca
+        //obliczoną wartość pola koła.Napisz program wykorzystujący funkcję pole_kola.
+        static double pole_kola(double r)
+        { return Math.PI * r * r; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pole koła o r = 5  to {0}", pole_kola(5));
+            Console.ReadKey(true);
+        }
+    }
+}
