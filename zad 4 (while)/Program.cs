0096425b2c13a37cc14f1d41b0980bd6330c7752
KubaSz123 <kubutor@gmail.com>
2025-06-03 21:13:18 +0200

    uzupelnione

---
+++ zad 4 (while)/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad_4__while_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int n, k, i, j;
+
+                Console.Write("Podaj n = ");
+                n = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Podaj k = ");
+                k = Convert.ToInt32(Console.ReadLine());
+
+                i = 1;
+                while (i <= n) // liczba wierszy
+                {
+                    j = 1;
+                    while (j <= k) // liczba kolumn (gwiazdek)
+                    {
+                        Console.Write("*");
+                        j++;
+                    }
+
+                    Console.WriteLine(""); // nowa linia
+                    i++;
+                }
+                Console.ReadKey(true); // pauza
+            }
+            }
+    }
+}
